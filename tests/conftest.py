de98dd61e4ecda31d13b03343298a93dd0aac577
hemarathinam <hemavathir214@gmail.com>
2025-02-18 21:32:42 -0500

    updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,11 +20,9 @@ def generate_test_data(num_records):
         b = Decimal(fake.random_number(digits=2)) if _ % 4 != 3 else Decimal(fake.random_number(digits=1))
         operation_name = fake.random_element(elements=list(operation_mappings.keys()))
         operation_func = operation_mappings[operation_name]
-        
         # Ensure b is not zero for divide operation to prevent division by zero in expected calculation
         if operation_func == divide:
             b = Decimal('1') if b == Decimal('0') else b
-        
         try:
             if operation_func == divide and b == Decimal('0'):
                 expected = "ZeroDivisionError"
@@ -34,7 +32,6 @@ def generate_test_data(num_records):
             expected = "ZeroDivisionError"
         
         yield a, b, operation_name, operation_func, expected
-
 def pytest_addoption(parser):
     parser.addoption("--num_records", action="store", default=5, type=int, help="Number of test records to generate")
 
@@ -49,3 +46,4 @@ def pytest_generate_tests(metafunc):
         # Modify parameters to fit test functions' expectations
         modified_parameters = [(a, b, op_name if 'operation_name' in metafunc.fixturenames else op_func, expected) for a, b, op_name, op_func, expected in parameters]
         metafunc.parametrize("a,b,operation,expected", modified_parameters)
+        
